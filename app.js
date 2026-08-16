55d2f3bc8b4aefede01f4857e96b68103a4cc3fe
coreqstudio <coreqstudio@gmail.com>
2026-08-16 22:02:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//const app = initializeApp(firebaseConfig);
 
 // ========== KHỞI TẠO FIREBASE ==========
 firebase.initializeApp(firebaseConfig);
